56cc9a0b2dee4efab7cd45f9abf7afcdde156048
Tobias Suckow <suckowbiz@users.noreply.github.com>
2016-11-21 07:42:23 +0000

    Update entrypoint.sh to apply recommendations of shell check (https://www.shellcheck.net/#)
    Bash improvements to avoid issues.

--- base/context/entrypoint.sh
+++ base/context/entrypoint.sh
@@ -8,21 +8,21 @@ readonly GROUP_MATCHES_EXACTLY=$(grep "^${GIVEN_GROUP}" /etc/group |grep ":${GIV
 if [ "${GROUP_MATCHES_EXACTLY}" = "1" ]
 then
   echo "Adding group: ${GIVEN_GROUP} (${GIVEN_GROUP_ID})..."
-  groupadd --gid ${GIVEN_GROUP_ID} --non-unique ${GIVEN_GROUP}
+  groupadd --gid "${GIVEN_GROUP_ID}" --non-unique "${GIVEN_GROUP}"
 fi
 
 readonly USER_MATCHES_EXACTLY=$(grep "^${GIVEN_USER}" /etc/passwd |grep ":${GIVEN_USER_ID}:"; echo $?)
 if [ "${USER_MATCHES_EXACTLY}" = "1" ]
 then
   echo "Adding user: ${GIVEN_USER}(${GIVEN_USER_ID}).."
-  useradd --create-home --non-unique --uid ${GIVEN_USER_ID} --gid ${GIVEN_GROUP_ID} ${GIVEN_USER}
+  useradd --create-home --non-unique --uid "${GIVEN_USER_ID}" --gid "${GIVEN_GROUP_ID}" "${GIVEN_USER}"
   if [ -n "${ENTRYPOINT_GROUPS}" ]
   then
     echo "Adding ${GIVEN_USER} to groups: ${ENTRYPOINT_GROUPS}..."
-    usermod -a -G ${ENTRYPOINT_GROUPS} ${GIVEN_USER}
+    usermod -a -G "${ENTRYPOINT_GROUPS}" "${GIVEN_USER}"
   fi
 else
-  if [ "$GIVEN_USER_ID" = "0" ]
+  if [ "${GIVEN_USER_ID}" = "0" ]
   then
     cp --recursive --no-dereference /etc/skel/. /root/
   fi
@@ -30,4 +30,4 @@ fi
 
 # 'gosu' is used to run the executable on behalf of the just created user. (gosu has 1:1 parity with "docker --user")
 # (idea.sh itself runs a foreground process utilizing 'exec')
-gosu ${GIVEN_USER_ID} $@
+gosu "${GIVEN_USER_ID}" "$@"
