ff2a48472b7247e843a281ada3804411ddd7d2d7
Tobias <tobias@suckow.biz>
2016-12-11 15:36:14 +0100

    Cleanup entrypoint.

--- base/base/context/entrypoint.sh
+++ base/base/context/entrypoint.sh
@@ -5,27 +5,16 @@ set -e
 
 # Apply a user/group id that enables the given user being the owner of new files.
 readonly GROUP_MATCHES_EXACTLY=$(grep "^${GIVEN_GROUP}" /etc/group |grep ":${GIVEN_GROUP_ID}:"; echo $?)
-if [ "${GROUP_MATCHES_EXACTLY}" = "1" ]
-then
-  #echo "Adding group: ${GIVEN_GROUP} (${GIVEN_GROUP_ID})..."
-  groupadd --gid "${GIVEN_GROUP_ID}" --non-unique "${GIVEN_GROUP}"
-fi
+test "${GROUP_MATCHES_EXACTLY}" = "1" && groupadd --gid "${GIVEN_GROUP_ID}" --non-unique "${GIVEN_GROUP}"
 
 readonly USER_MATCHES_EXACTLY=$(grep "^${GIVEN_USER}" /etc/passwd |grep ":${GIVEN_USER_ID}:"; echo $?)
 if [ "${USER_MATCHES_EXACTLY}" = "1" ]
 then
   #echo "Adding user: ${GIVEN_USER}(${GIVEN_USER_ID}).."
   useradd --create-home --non-unique --uid "${GIVEN_USER_ID}" --gid "${GIVEN_GROUP_ID}" "${GIVEN_USER}"
-  if [ -n "${ENTRYPOINT_GROUPS}" ]
-  then
-    #echo "Adding ${GIVEN_USER} to groups: ${ENTRYPOINT_GROUPS}..."
-    usermod -a -G "${ENTRYPOINT_GROUPS}" "${GIVEN_USER}"
-  fi
+  test -n "${ENTRYPOINT_GROUPS}" && usermod -a -G "${ENTRYPOINT_GROUPS}" "${GIVEN_USER}"
 else
-  if [ "${GIVEN_USER_ID}" = "0" ]
-  then
-    cp --recursive --no-dereference /etc/skel/. /root/
-  fi
+  test "${GIVEN_USER_ID}" = "0" && cp --recursive --no-dereference /etc/skel/. /root/
 fi
 
 # 'gosu' is used to run the executable on behalf of the just created user. (gosu has 1:1 parity with "docker --user")
