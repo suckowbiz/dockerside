56c5e39cdaadb08f8e646dc99ee04ec07ef723c0
Tobias <tobias@suckow.biz>
2016-12-14 19:50:47 +0100

    Introduce entrypoing workdir to switch to if set.

--- base/base/context/entrypoint.sh
+++ base/base/context/entrypoint.sh
@@ -4,17 +4,25 @@
 set -e
 
 # Apply a user/group id that enables the given user being the owner of new files.
-readonly GROUP_MATCHES_EXACTLY=$(grep "^${GIVEN_GROUP}" /etc/group |grep ":${GIVEN_GROUP_ID}:"; echo $?)
-test "${GROUP_MATCHES_EXACTLY}" = "1" && groupadd --gid "${GIVEN_GROUP_ID}" --non-unique "${GIVEN_GROUP}"
+readonly NO_SUCH_GROUP_EXISTS=$(grep "^${GIVEN_GROUP}" /etc/group |grep ":${GIVEN_GROUP_ID}:"; echo $?)
+if [[ "${NO_SUCH_GROUP_EXISTS}" = "1" ]]; then
+    groupadd --gid "${GIVEN_GROUP_ID}" --non-unique "${GIVEN_GROUP}"
+fi
 
-readonly USER_MATCHES_EXACTLY=$(grep "^${GIVEN_USER}" /etc/passwd |grep ":${GIVEN_USER_ID}:"; echo $?)
-if [ "${USER_MATCHES_EXACTLY}" = "1" ]
-then
-  #echo "Adding user: ${GIVEN_USER}(${GIVEN_USER_ID}).."
-  useradd --create-home --non-unique --uid "${GIVEN_USER_ID}" --gid "${GIVEN_GROUP_ID}" "${GIVEN_USER}"
-  test -n "${ENTRYPOINT_GROUPS}" && usermod -a -G "${ENTRYPOINT_GROUPS}" "${GIVEN_USER}"
+readonly NO_SUCH_USER_EXISTS=$(grep "^${GIVEN_USER}" /etc/passwd |grep ":${GIVEN_USER_ID}:"; echo $?)
+if [[ "${NO_SUCH_USER_EXISTS}" = "1" ]]; then
+    useradd --create-home --non-unique --uid "${GIVEN_USER_ID}" --gid "${GIVEN_GROUP_ID}" "${GIVEN_USER}"
+    if [[ -n "${ENTRYPOINT_GROUPS}" ]]; then
+        usermod -a -G "${ENTRYPOINT_GROUPS}" "${GIVEN_USER}"
+    fi
 else
-  test "${GIVEN_USER_ID}" = "0" && cp --recursive --no-dereference /etc/skel/. /root/
+if [[ "${GIVEN_USER_ID}" = "0" ]]; then
+        cp --recursive --no-dereference /etc/skel/. /root/
+    fi
+fi
+
+if [[ ! "ENTRYPOINT_WORKDIR" = "" ]]; then
+    cd "${ENTRYPOINT_WORKDIR}"
 fi
 
 # 'gosu' is used to run the executable on behalf of the just created user. (gosu has 1:1 parity with "docker --user")
