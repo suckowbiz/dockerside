4ba0c2a3e5bf31632ce61f51ad0388f94fa15ce4
Tobias <tobias@suckow.biz>
2017-01-09 23:58:21 +0100

    Improved nginx user replacement to cover cases where nginx is set instead of www-data.

--- nginx/base/context/start.sh
+++ nginx/base/context/start.sh
@@ -3,6 +3,6 @@
 # Intentionally added to override entrypoint.sh to enable post processing of user names.
 
 # Set the nginx workers user to preserver file rights.
-sed --in-place "s/user www-data;/user ${GIVEN_USER} ${GIVEN_GROUP};/g" /etc/nginx/nginx.conf
+sed --in-place "s/user .*;/user ${GIVEN_USER} ${GIVEN_GROUP};/g" /etc/nginx/nginx.conf
 
 exec "$@"
