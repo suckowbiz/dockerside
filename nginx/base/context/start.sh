3b4b1c6ae5b0310693490f4ee9514b4ee2bd2a2a
Tobias <tobias@suckow.biz>
2017-04-06 18:38:18 +0200

    Replaced MAINTAINER to avoid failures for deprecation and applied shellcheck suggestions to improve quality.

--- nginx/base/context/start.sh
+++ nginx/base/context/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #
 # Intentionally added to override entrypoint.sh to enable post processing of user names.
+set -e
 
 # Set the nginx workers user to preserver file rights.
 sed --in-place "s/user .*;/user ${GIVEN_USER} ${GIVEN_GROUP};/g" /etc/nginx/nginx.conf
