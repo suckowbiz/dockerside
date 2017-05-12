42c7b9426b344c66abacd49fe7835e0da9ad3bec
Tobias <tobias@suckow.biz>
2017-05-12 19:36:12 +0200

    Upgrade git 2.12.2 -> 2.13.0. Compose file to vers 3. Refactoring.

---
+++ vscode/setup-devops-plugins.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+code --install-extension autocomplete-ansible
+code --install-extension vscode-docker
+code --install-extension jinja
+code --install-extension markdownlint
+code --install-extension shellcheck
+code --install-extension vscode-yaml-validation
