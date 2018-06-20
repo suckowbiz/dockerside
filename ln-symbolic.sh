121faa6116b7e51f518b3e5774e5795a764866bb
Tobias <tobias@suckow.biz>
2018-06-20 20:20:58 +0200

    NFS no longe makes sense since no user mapping fs is in place.

--- ln-symbolic.sh
+++ ln-symbolic.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for app in chrome clementine code gimp goland keepass2 nfs tor caddy git supertux; do
+for app in chrome chromix clementine code gimp goland keepass2 tor caddy git supertux; do
   sudo ln --force --symbolic "${PWD}/${app}/${app}" "/usr/local/bin/${app}"
 done
 
