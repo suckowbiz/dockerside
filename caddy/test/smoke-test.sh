53e60e879a65f3c1ebfe313b30eab7340f396cc2
Tobias <tobias@suckow.biz>
2020-07-04 10:11:24 +0200

    Upgrade caddy 1.0.4 -> 2.1.1

--- caddy/test/smoke-test.sh
+++ caddy/test/smoke-test.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
+set -e
 
 readonly HERE=$(dirname "$(readlink -f "$0")")
 cd $HERE
 ../caddy
 sleep 1
 
-readonly ACTUAL_RESULT=$(curl --silent localhost:2015)
-readonly EXPECTED_RESULT='401 Unauthorized'
-readonly TEST_RESULT=$(echo "${ACTUAL_RESULT}" |grep --silent "${EXPECTED_RESULT}" && echo $?)
-if [[ "${TEST_RESULT}" = "0" ]]; then
+readonly ACTUAL_RESULT=$(curl --silent --output /dev/null --write-out "%{http_code}" localhost:2015)
+readonly EXPECTED_RESULT='401'
+if [[ "${ACTUAL_RESULT}" = "${EXPECTED_RESULT}" ]]; then
     echo "Success: Requesting caddy returned expected: ${EXPECTED_RESULT}."
 else
     echo "Failure: Requesting caddy did not return expected: ${EXPECTED_RESULT}."
